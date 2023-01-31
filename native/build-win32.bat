2cec7d6e133b06f566433adacc75e8244de97a23
Jire <thomasgnappo@gmail.com>
2023-01-31 03:32:06 -0500

    Added prebuilt `detours` and fixed `native/build.bat` scripts

---
+++ native/build-win32.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\CMake\bin\cmake.exe" -G "Visual Studio 16 2019" -A Win32 -S . -B "build32"
+"C:\Program Files\CMake\bin\cmake.exe" --build build32 --config Release
---
+++ native/build-win64.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\CMake\bin\cmake.exe" -G "Visual Studio 16 2019" -A x64 -S . -B "build64"
+"C:\Program Files\CMake\bin\cmake.exe" --build build64 --config Release
